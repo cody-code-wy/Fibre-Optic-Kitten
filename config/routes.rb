3057584645d004d836fd1b875b47a4f632f7862a
William Young <william@werl.me>
2016-07-16 16:58:58 -0700

    Add Posts topic route and basic implementation
    This will cause `/posts/:topic` to list all available subtopics/static
    pages. This is needed to allow users to navigate inside a topic.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/posts', to: 'blog_posts#index'
+  get '/posts/:topic', to: 'blog_posts#topic'
 
 end
