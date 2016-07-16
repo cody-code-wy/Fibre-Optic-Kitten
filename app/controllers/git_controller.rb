80f9ae700f110d6b866c7a18a9ec57833cdf2d64
werl <peter@werl.me>
2016-07-16 15:12:33 -0700

    Add git/pull route w/ basic implementation
    This will clone a given repository, deleting the old repo. This is
    needed to update the content of the blog.
    
    Deleting and cloning is the simplest implementation of this feature. In
    the future it could be improved by using git pull.

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -1,2 +1,23 @@
+require 'json'
+require 'git'
+require 'fileutils'
+
 class GitController < ApplicationController
+
+  def pull
+    conf = get_config
+    puts conf
+    if conf
+      clone_repo(conf['repo'], conf['branch'])
+    end
+    head 200, content_type: "text/html"
+  end
+
+  def clone_repo (repo, branch)
+    if Dir.exists?('blog content')
+      FileUtils.rm_rf('blog content')
+    end
+    Git.clone(repo, branch, path: 'blog content/')
+  end
+
 end
