c30157609df603a5bc98022b57682a3894ccb459
William Young <william@werl.me>
2016-07-16 18:57:29 -0700

    Add helper method to parse markdown files
    Adds a helper method `parse_markdown` to BlogPosts that reads markdown
    files and creates html rederable output (that will not be escaped by
    rails automatically)
    
    Includes stub methods for getting render options, and getting markdown
    options.

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -1,2 +1,23 @@
 module BlogPostsHelper
+
+  require 'redcarpet'
+
+  def parse_markdown(file_path)
+    @renderer ||= Redcarpet::Render::HTML.new(render_options)
+    @markdown ||= Redcarpet::Markdown.new(@renderer, markdown_options)
+    file = File.open(file_path, 'r') # Open the md file to read
+    file_content = file.read # Read all lines
+    file.close # And we are done
+    @markdown.render(file_content).html_safe
+  end
+
+  private
+
+  def render_options
+    {} # TODO: get these from config
+  end
+
+  def markdown_options
+    {} # TODO: get these from config
+  end
 end
