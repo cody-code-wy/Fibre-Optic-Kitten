0644accd90c53927b1f720436cc095c26bc7eb82
werl <peter@werl.me>
2016-07-16 18:14:55 -0700

    Remove /git route
    Removed the /git route because it is not needed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'git', to: :show, controller: 'git'
   get '/git/pull', to: 'git#pull'
 
 end
