4aaea5ee56639defd6a6cc63c2c759818f9bd41b
William Young <william@werl.me>
2016-07-16 19:00:48 -0700

    Add Topic subcontent route and basic imlementation
    This will cause `/posts/:topic/:subcontent` to, if a directory, list all
    files in the directory, or if a markdown file render the markdown file.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -9,4 +9,8 @@ class BlogPostsController < ApplicationController
   def topic()
     @topic_root = "#{@@post_root}/#{params[:topic]}"
   end
+
+  def subcontent()
+    @content_root = "#{@@post_root}/#{params[:topic]}/#{params[:subcontent]}"
+  end
 end
