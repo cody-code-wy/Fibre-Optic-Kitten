2a2bebef7c60638ba4f84b88176b4e217998fdc7
William Young <william@werl.me>
2016-07-16 19:15:04 -0700

    Move directory listing to partial
    this DRYs up code,

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -3,15 +3,19 @@ class BlogPostsController < ApplicationController
   @@post_root = 'blog content/master/Posts'
 
   def index
-    @post_root = @@post_root
+    @dir_to_list = @@post_root
+    @context = 'posts'
   end
 
   def topic
-    @topic_root = "#{@@post_root}/#{params[:topic]}"
+    @dir_to_list = "#{@@post_root}/#{params[:topic]}"
+    @context = params[:topic]
   end
 
   def subcontent
     @content_root = "#{@@post_root}/#{params[:topic]}/#{params[:subcontent]}"
+    @dir_to_list = @content_root # FIXME
+    @context = params[:subcontent]
   end
 
   def post()
