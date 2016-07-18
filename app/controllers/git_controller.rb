6de2b047dfbff989e8c0d1927c7bf98ac033d861
werl <peter@werl.me>
2016-07-17 19:50:45 -0700

    Add git/pull route w/ implementation
    This will clone a given repository or pull if it currently exists.
    
    This is to get the latest blog version from the git repo.

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -5,12 +5,17 @@ require 'fileutils'
 class GitController < ApplicationController
 
   def pull
+<<<<<<< HEAD
     conf = get_config
     puts conf
     if conf
       clone_repo(conf['repo'], conf['branch'])
     end
     head 200, content_type: "text/html"
+=======
+    update_repo('blog.conf')
+    head 200, content_type: 'text/html'
+>>>>>>> 68321a0... fixup! Add git/pull route w/ basic implementation
   end
 
   def clone_repo (repo, branch)
@@ -20,4 +25,23 @@ class GitController < ApplicationController
     Git.clone(repo, branch, path: 'blog content/')
   end
 
+<<<<<<< HEAD
+=======
+  def pull_repo (repo, branch)
+    g = Git.open("blog content/#{branch}", log: Logger.new(STDOUT))
+    g.pull(repo, branch)
+  end
+
+  def update_repo(config)
+    conf = get_config(config)
+    if conf
+      if File.directory?("blog content/#{conf['branch']}")
+        pull_repo(conf['repo'], conf['branch'])
+      else
+        clone_repo(conf['repo'], conf['branch'])
+      end
+    end
+  end
+
+>>>>>>> 68321a0... fixup! Add git/pull route w/ basic implementation
 end
