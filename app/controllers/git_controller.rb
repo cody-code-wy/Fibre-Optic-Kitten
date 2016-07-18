85ebc9242493158081b5b8301104997bb692742b
werl <peter@werl.me>
2016-07-17 18:10:29 -0700

    Implement pulling repositories
    Added 'pull_repo' and 'update_repo' methods. Clones if no
    repo exists otherwise pulls. Safer then constintly recloning.

--- app/controllers/git_controller.rb
+++ app/controllers/git_controller.rb
@@ -5,28 +5,17 @@ require 'fileutils'
 class GitController < ApplicationController
 
   def pull
-<<<<<<< HEAD
-    conf = get_config
-    puts conf
-    if conf
-      clone_repo(conf['repo'], conf['branch'])
-    end
-    head 200, content_type: "text/html"
-=======
     update_repo('blog.conf')
     head 200, content_type: 'text/html'
->>>>>>> 68321a0... fixup! Add git/pull route w/ basic implementation
   end
 
   def clone_repo (repo, branch)
-    if Dir.exists?('blog content')
+    if File.exists?('blog content')
       FileUtils.rm_rf('blog content')
     end
     Git.clone(repo, branch, path: 'blog content/')
   end
 
-<<<<<<< HEAD
-=======
   def pull_repo (repo, branch)
     g = Git.open("blog content/#{branch}", log: Logger.new(STDOUT))
     g.pull(repo, branch)
@@ -35,13 +24,11 @@ class GitController < ApplicationController
   def update_repo(config)
     conf = get_config(config)
     if conf
-      if File.directory?("blog content/#{conf['branch']}")
+      if Dir.exist?("blog content/#{conf['branch']}")
         pull_repo(conf['repo'], conf['branch'])
       else
         clone_repo(conf['repo'], conf['branch'])
       end
     end
   end
-
->>>>>>> 68321a0... fixup! Add git/pull route w/ basic implementation
 end
