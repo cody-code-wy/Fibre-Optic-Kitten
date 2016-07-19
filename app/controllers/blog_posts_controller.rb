cb74f3243bb1619fa16f3079487db1869e4f8124
William Young <william@werl.me>
2016-07-19 13:31:58 -0700

    Change post_root into instance variable
    This is the ideomatic ruby way to achive this result, and as such it
    will be used.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,23 +1,24 @@
 class BlogPostsController < ApplicationController
-
-  @@post_root = 'blog content/master/Posts'
+  def initialize
+    @post_root = 'blog content/master/Posts'
+  end
 
   def index
-    @dir_to_list = @@post_root
+    @dir_to_list = @post_root
     @context = 'posts'
   end
 
   def topic
-    @dir_to_list = "#{@@post_root}/#{params[:topic]}"
+    @dir_to_list = "#{@post_root}/#{params[:topic]}"
     @context = params[:topic]
   end
 
   def subcontent
-    @content_root = "#{@@post_root}/#{params[:topic]}/#{params[:subcontent]}"
+    @content_root = "#{@post_root}/#{params[:topic]}/#{params[:subcontent]}"
     @context = params[:subcontent]
   end
 
   def post
-    @content_root = "#{@@post_root}/#{params[:topic]}/#{params[:subtopic]}/#{params[:post]}"
+    @content_root = "#{@post_root}/#{params[:topic]}/#{params[:subtopic]}/#{params[:post]}"
   end
 end
