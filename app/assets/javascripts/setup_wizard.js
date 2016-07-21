7bd04da2f811e3bd61e2bc38da19ac7e66b06344
Peter Lewis <peter@werl.me>
2016-07-21 12:48:18 -0600

    Add indentation and change showSignup to showBlogConf
    The indentaion was for better readability. The name change is to
    make the function more descriptive

--- app/assets/javascripts/setup_wizard.js
+++ app/assets/javascripts/setup_wizard.js
@@ -5,10 +5,10 @@ function scrollTo(id) {
     }, 1000);
 }
 
-function showSignup() {
-    $("#account").removeClass("hidden")
+function showBlogConf() {
+    $("#blog").removeClass("hidden")
 
-    scrollTo('account')
+    scrollTo('blog')
 }
 
 function clearFields() {
@@ -21,53 +21,64 @@ $(() => {
 
     // Clone Button
     $('#clone-url').on('click', () => {
-    $('#github-username').addClass('hidden')
-$('#github-repos').addClass('hidden')
-$('#account').addClass('hidden')
-$('#clone').removeClass('hidden')
-clearFields()
-scrollTo('clone')
-})
-
-// Clone Button Click
-$("#clone .continue-button").on('click', () => {
-    showSignup()
-})
-// Github Button
-$('#github').on('click', () => {
-    $('#clone').addClass('hidden')
-$('#github-repos').addClass('hidden')
-$('#account').addClass('hidden')
-$('#github-username').removeClass('hidden')
-clearFields()
-scrollTo('github-username')
-})
-
-// List Repos Button
-$('#github-username .continue-button').on('click', () => {
-    var uname = $('input[name="github-uname"]').val()
-    $.getJSON('https://api.github.com/users/' + uname + '/repos', (data) => {
-
-    $('.repos').empty()
-$('#github-repos').removeClass('hidden')
-
-scrollTo('github-repos')
-
-data.forEach((repo) => {
-    $('<div>').text(repo.name).addClass("repo").data("github-url", repo.clone_url).appendTo($(".repos")).on('click', (event) => {
-    $('input[name="github-url"]').val($(event.target).data("github-url"))
-$('.repos .repo').removeClass('highlight')
-$(event.target).addClass('highlight')
-showSignup()
-})
-})
-
-}).fail(() => {
-
-    $("<div>").text("Nonexistant User (click to close)").addClass("error").appendTo('#github-username .modal').on('click', (event) => {
-    $(event.target).remove()
-})
-})
-})
-
+        $('#github-username').addClass('hidden')
+        $('#github-repos').addClass('hidden')
+        $('#account').addClass('hidden')
+        $('#clone').removeClass('hidden')
+        $('#blog').addClass('hidden')
+        clearFields()
+        scrollTo('clone')
+    })
+    
+    // Clone Button Click
+    $("#clone .continue-button").on('click', () => {
+        showBlogConf()
+    })
+    // Github Button
+    $('#github').on('click', () => {
+        $('#clone').addClass('hidden')
+        $('#github-repos').addClass('hidden')
+        $('#account').addClass('hidden')
+        $('#github-username').removeClass('hidden')
+        $('#blog').addClass('hidden')
+        clearFields()
+        scrollTo('github-username')
+    })
+    
+    // List Repos Button
+    $('#github-username .continue-button').on('click', () => {
+        var uname = $('input[name="github-uname"]').val()
+        $.getJSON('https://api.github.com/users/' + uname + '/repos', (data) => {
+    
+            $('.repos').empty()
+            $('#github-repos').removeClass('hidden')
+    
+            clearFields('github-repos')
+    
+            data.forEach((repo) => {
+            $('<div>').text(repo.name).addClass("repo").data("github-url", repo.clone_url).appendTo($(".repos")).on('click', (event) => {
+                $('input[name="github-url"]').val($(event.target).data("github-url"))
+                $('.repos .repo').removeClass('highlight')
+                $(event.target).addClass('highlight')
+                showBlogConf()
+            })
+        })
+    
+        }).fail(() => {
+    
+            $("<div>").text("Nonexistant User (click to close)").addClass("error").appendTo('#github-username .modal').on('click', (event) => {
+                $(event.target).remove()
+            })
+        })
+    })
+    
+    $('#blog .continue-button').on('click', () => {
+        $('#account').removeClass('hidden')
+    
+        scrollTo('account')
+    })
+    
+    $('#account .continue-button').on('click', () => {
+        $('form').submit();
+    })
 })
