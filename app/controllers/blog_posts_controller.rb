66ca88e7fe581fc7fbd074fc848d8d641c2be748
William Young <william@werl.me>
2016-07-21 14:13:46 -0700

    Redirect to wizard if repo.conf does not exist.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -6,17 +6,21 @@ class BlogPostsController < ApplicationController
 
   def index
     @content_root = @post_root
+    redirect_to '/wizard' unless get_config('repo.conf')
   end
 
   def topic
     @content_root = "#{@post_root}/#{params[:topic]}"
+    redirect_to '/wizard' unless get_config('repo.conf')
   end
 
   def subcontent
     @content_root = "#{@post_root}/#{params[:topic]}/#{params[:post]}"
+    redirect_to '/wizard' unless get_config('repo.conf')
   end
 
   def post
     @content_root = "#{@post_root}/#{params[:topic]}/#{params[:subtopic]}/#{params[:post]}"
+    redirect_to '/wizard' unless get_config('repo.conf')
   end
 end
